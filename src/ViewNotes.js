23330098a9559cc5d075bfdf48e8c043745678e4
kumarsauravks <kumarsauravks@gmail.com>
2021-01-03 17:42:18 +0530

    date-picker fixes

--- src/ViewNotes.js
+++ src/ViewNotes.js
@@ -25,9 +25,13 @@ function ViewNotes() {
             for (let i = 0; i < newData.length; i++) {
                 tempData.push(JSON.parse(newData[i]))
             }
+            setShowData(tempData);
+            console.log(tempData)
         }
-        setShowData(tempData);
-        console.log(tempData)
+        else {
+            setShowData(null);
+        }
+
     }, [newData]);
 
     function deleteHandler(d1) {
@@ -45,12 +49,12 @@ function ViewNotes() {
         }
         setShowData(newData);
     }
-    let day=new Date().getDate();
-    let month=new Date().getMonth();
-    let year=new Date().getFullYear();
-    let dateStr=(year+"-"+month+1+"-"+day);
-    let [fromDate,setFromDate]=useState(new Date(dateStr));
-    let [toDate,setToDate]=useState(new Date());
+    let day = new Date().getDate();
+    let month = new Date().getMonth();
+    let year = new Date().getFullYear();
+    let dateStr = (year + "-" + month + 1 + "-" + day);
+    let [fromDate, setFromDate] = useState(new Date(dateStr));
+    let [toDate, setToDate] = useState(new Date());
 
     function editHandler(d1) {
 
@@ -62,33 +66,34 @@ function ViewNotes() {
 
         return [d[0], d[1], d[2], d[3]].join(' ');
     }
-    function filterHandler(){
-        let tempData=[];
-        let tempData1=[];
-        for(let i=0;i<newData.length;i++){
+    function filterHandler() {
+        let tempData = [];
+        let tempData1 = [];
+        for (let i = 0; i < newData.length; i++) {
             tempData.push(JSON.parse(newData[i]));
         }
-        tempData.filter((x)=>{
-            let tempComingDate=new Date(x.date.split("T")[0]).getTime();
+        tempData.filter((x) => {
+            let tempComingDate = new Date(x.date.split("T")[0]).getTime();
             console.log(fromDate.getTime())
-                console.log(tempComingDate)
-                console.log(toDate.getTime())
-            if(tempComingDate>=fromDate.getTime() && tempComingDate<=toDate.getTime()){
+            console.log(tempComingDate)
+            console.log(toDate.getTime())
+            if (tempComingDate >= fromDate.getTime() && tempComingDate <= toDate.getTime()) {
                 console.log("inside If")
                 tempData1.push(x);
-            }  
+            }
         })
         console.log(tempData);
         console.log(tempData1);
         setShowData(tempData1);
     }
+
     return (
         <div className='container my-3'>
             <h1 className="viewNoteHead">Your Notes</h1>
             <div className="btn-box">
-                <label className="mx-2">From Date <DatePicker showYearDropdown scrollableMonthYearDropdown dateFormat="dd/MM/yyyy" selected={fromDate} onChange={date=>{setFromDate(date)}} /></label>
-                <label className="mx-2">To Date <DatePicker showYearDropdown scrollableMonthYearDropdown dateFormat="dd/MM/yyyy" selected={toDate} onChange={date=>setToDate(date)} /></label>
-                <button onClick={()=>filterHandler()} className="btn btn-secondary m-1 d-inline-block" >Filter</button>
+                <label className="mx-2">From Date <DatePicker showYearDropdown scrollableMonthYearDropdown dateFormat="dd/MM/yyyy" selected={fromDate} onChange={date => { setFromDate(date) }} /></label>
+                <label className="mx-2">To Date <DatePicker showYearDropdown scrollableMonthYearDropdown dateFormat="dd/MM/yyyy" selected={toDate} onChange={date => setToDate(date)} /></label>
+                <button onClick={() => filterHandler()} className="btn btn-secondary m-1 d-inline-block" >Filter</button>
             </div>
             <div className="row container-fluid" id="notes">
                 {
@@ -103,7 +108,7 @@ function ViewNotes() {
                                 </div>
                             </div>
                         )
-                        ) : <div>Nothing to show! Please add a Note to populate the list</div>
+                        ) : <div className="noData">Nothing to show! Please add a Note to populate the list</div>
                 }
             </div>
         </div>
